f458d0ee125a7a1281e9d561ec002cb6dfa83bfd
Viviane Pereira <vivianecruz01@hotmail.com>
2015-06-02 11:48:34 -0300

    Update fila.c

--- Atividade_10/fila.c
+++ Atividade_10/fila.c
@@ -10,38 +10,43 @@ float vet[N];
 
 Fila* cria (void)
 {
-Fila* f = (Fila*) malloc(sizeof(Fila));
-f->ini = f->fim = 0; // inicializa fila vazia 
-return f;
+  Fila* f = (Fila*) malloc(sizeof(Fila));
+  f->ini = f->fim = 0; // inicializa fila vazia 
+  return f;
 }
+
 void insere (Fila* f, float v)
 {
-if (incr(f->fim) == f->ini) { // fila cheia: capacidade esgotada
+  if (incr(f->fim) == f->ini) 
+  { // fila cheia: capacidade esgotada
+  printf("Capacidade da fila estourou.\n");
+  exit(1); // aborta programa *
+  }
+  // insere elemento na próxima posição livre 
+  f->vet[f->fim] = v;
+  f->fim = incr(f->fim);
+}
 
-printf("Capacidade da fila estourou.\n");
-exit(1); // aborta programa *
-}
-// insere elemento na próxima posição livre 
-f->vet[f->fim] = v;
-f->fim = incr(f->fim);
-}
 float retira (Fila* f)
 {
-float v;
-if (vazia(f)) {
-printf("Fila vazia.\n");
-exit(1); /* aborta programa */
+  float v;
+  if (vazia(f)) 
+  {
+    printf("Fila vazia.\n");
+    exit(1); /* aborta programa */
+  }
+  /* retira elemento do início */
+  v = f->vet[f->ini];
+  f->ini = incr(f->ini);
+  return v;
 }
-/* retira elemento do início */
-v = f->vet[f->ini];
-f->ini = incr(f->ini);
-return v;
-}
+
 int vazia (Fila* f)
 {
-return (f->ini == f->fim);
+  return (f->ini == f->fim);
 }
+
 void libera (Fila* f)
 {
-free(f);
+  free(f);
 }
