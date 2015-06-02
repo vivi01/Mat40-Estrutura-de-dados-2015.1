0a94027c05f81db9e8051aad1108ca80b446106d
Viviane Pereira <vivianecruz01@hotmail.com>
2015-06-02 11:52:27 -0300

    Update main.c

--- Atividade_10/main.c
+++ Atividade_10/main.c
@@ -1,17 +1,19 @@
-/* Módulo para ilustrar utilização da fila */
+/* Programa de  teste da fila */
 #include <stdio.h>
 #include "fila.h"
+
+
 int main (void)
 {
-Fila* f = cria();
-insere(f,20.0);
-insere(f,20.8);
-insere(f,21.2);
-insere(f,24.3);
-printf("Primeiro elemento: %f\n", retira(f));
-printf("Segundo elemento: %f\n", retira(f));
-printf("Configuracao da fila:\n");
-imprime(f);
-libera(f);
-return 0;
+  Fila* f = cria();
+  insere(f,45.0);
+  insere(f,30.8);
+  insere(f,91.7);
+  insere(f,74.9);
+  printf("Primeiro elemento: %f\n", retira(f));
+  printf("Segundo elemento: %f\n", retira(f));
+  printf("Configuracao da fila:\n");
+  imprime(f);
+  libera(f);
+  return 0;
 }
