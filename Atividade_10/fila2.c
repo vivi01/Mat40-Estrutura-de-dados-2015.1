085f51cf4cec4df79d6e384cfc2eb93dfa5cc35e
Viviane Pereira <vivianecruz01@hotmail.com>
2015-06-02 11:45:11 -0300

    Update fila2.c

--- Atividade_10/fila2.c
+++ Atividade_10/fila2.c
@@ -5,61 +5,70 @@
 struct no {
 float info;
 struct no* prox;
-};c
+};
+
 struct fila {
 No* ini;
 No* fim;
 };
+
 Fila* cria (void)
 {
-Fila* f = (Fila*) malloc(sizeof(Fila));
-f->ini = f->fim = NULL;
-return f;
+  Fila* f = (Fila*) malloc(sizeof(Fila));
+  f->ini = f->fim = NULL;
+  return f;
 }
+
 void insere (Fila* f, float v)
 {
-f->fim = ins_fim(f->fim,v);
-if (f->ini==NULL) /* fila antes vazia? */
-f->ini = f->fim;
+  f->fim = ins_fim(f->fim,v);
+  if (f->ini==NULL) /* fila antes vazia? */
+  f->ini = f->fim;
 }
+
 float retira (Fila* f)
 {
-float v;
-if (vazia(f)) {
-printf("Fila vazia.\n");
-exit(1); /* aborta programa */
-}
+  float v;
+  if (vazia(f))
+  {
+    printf("Fila vazia.\n");
+    exit(1); /* aborta programa */
+  }
 v = f->ini->info;
 f->ini = ret_ini(f->ini);
 if (f->ini == NULL) /* fila ficou vazia? */
 f->fim = NULL;
 return v;
 }
+
 int vazia (Fila* f)
 {
-return (f->ini==NULL);
+  return (f->ini==NULL);
 }
+
 void libera (Fila* f)
 {
-No* q = f->ini;
-while (q!=NULL) {
-No* t = q->prox;
-free(q);
-q = t;
-}
-free(f);
+  No* q = f->ini;
+  while (q!=NULL) 
+  {
+    No* t = q->prox;
+    free(q);
+    q = t;
+  }
+  free(f);
 }
 /* imprime: versão com vetor */
 void imprime (Fila* f)
 {
-int i;
-for (i=f->ini; i!=f->fim; i=incr(i))
-printf("%f\n",f->vet[i]);
+  int i;
+  for (i=f->ini; i!=f->fim; i=incr(i))
+  printf("%f\n",f->vet[i]);
 }
+
 /* imprime: versão com lista */
 void imprime (Fila* f)
 {
-No* q;
-for (q=f->ini; q!=NULL; q=q->prox)
-printf("%f\n",q->info);
+  No* q;
+  for (q=f->ini; q!=NULL; q=q->prox)
+  printf("%f\n",q->info);
 }
