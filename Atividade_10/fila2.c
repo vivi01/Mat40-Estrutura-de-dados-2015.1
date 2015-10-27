4c1069231affc2572879046d78bcd91327c58d69
Viviane Pereira <vivianecruz01@hotmail.com>
2015-10-27 17:10:25 -0300

    Update fila2.c

--- Atividade_10/fila2.c
+++ Atividade_10/fila2.c
@@ -1,12 +1,16 @@
-//Função para criar uma fila
+#include<stdio.h>
+#include<stdlib.h>
+#include<fila.h>
+
+//FunÃ§Ã£o para criar uma fila
 Fila* cria (void)
 {
   Fila* f = (Fila*) malloc(sizeof(Fila));
   f->ini = NULL;
-	 f->fim = NULL;
+  f->fim = NULL;
   return f;
 }
-// função para inserir na fila
+// funÃ§Ã£o para inserir na fila
 void insere (Fila* f, float v)
 {
 	No* novo = (No*) malloc(sizeof(No));
@@ -18,31 +22,31 @@ void insere (Fila* f, float v)
 	f->ini = novo;
 	f->fim = novo;
 }
-// função para remover o primeiro elemento da fila
+// funÃ§Ã£o para remover o primeiro elemento da fila
 float retira (Fila* f)
 {
 	No* l;
-  float v;
-  if (vazia(f))
-  {
-    printf("Fila vazia.\n");
-    exit(1); /* aborta programa */
-  }
-  l = f->ini;
-  v = l->info;
-  f->ini = l->prox;
-  if (f->ini == NULL) 
-    f->fim = NULL;
-  free(l);
+	float v;
+      if (vazia(f))
+       {
+       	 printf("Fila vazia.\n");
+       	 exit(1); /* aborta programa */
+       	 }
+       l = f->ini;
+       v = l->info;
+       f->ini = l->prox;
+       if (f->ini == NULL) 
+       f->fim = NULL;
+       free(l);
 	return v;
 }
 
-// função para verificar se a fila esta vazia
+// funÃ§Ã£o para verificar se a fila esta vazia
 int vazia (Fila* f)
 {
   return (f->ini==NULL);
 }
-//função para desalocar a fila
+//funÃ§Ã£o para desalocar a fila
 void libera (Fila* f)
 {
   No* q = f->ini;
@@ -54,7 +58,7 @@ void libera (Fila* f)
   }
   free(f);
 }
-//função para imprimir
+//funÃ§Ã£o para imprimir
 void imprime (Fila* f)
 {
   No* q;
